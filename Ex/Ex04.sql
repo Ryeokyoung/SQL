7efa1233e19c184357c096b367032f2fa2c4d223
Ryeokyoung <ryeokyoungk@gmail.com>
2022-05-18 13:41:47 +0900

    2022-05-18

--- Ex/Ex04.sql
+++ Ex/Ex04.sql
@@ -129,6 +129,119 @@ where (department_id, salary) in ( select department_id,
                                    from employees
                                    group by department_id);
                                    
-                                   
+--각 부서별로 최고급여를 받는 사원이름을 출력하세요
+--where절로 구하기
+select  department_id,
+        max(salary)
+from employees
+group by department_id;
+
+
+select  first_name,
+        salary,
+        department_id,
+        email
+from employees
+where (department_id, salary) in ( select  department_id,
+                                           max(salary)
+                                   from employees
+                                   group by department_id  );
+
+--각 부서별로 최고급여를 받는 사원이름을 출력하세요
+--테이블로 구하기
+
+select  e.first_name,
+        e.salary,
+        e.department_id,
+        s.department_id,
+        s.maxSalary
+from employees e, (select department_id, 
+                          max(salary) maxSalary
+                   from employees
+                   group by department_id) s  
+where e.department_id = s.department_id
+and e.salary = s.maxSalary;
+
+-------------------------------------------------
+--rownum
+
+--급여를 가장 많이 받는 5명의 직원의 이름을 출력하시오.
+--정렬을 하면 rownum이 섞인다 -->정렬을 하고 rownum 을 준다
+select  rownum,
+        employee_id,
+        first_name,
+        salary
+from employees   -- 미리 정렬되어 있는 테이블이면 rownum이 섞이지 않는다
+order by salary desc;
+
+-->정렬(정렬된 테이블 사용)하고 rownum을 준다
+select  rownum,
+        ot.employee_id,
+        ot.first_name,
+        ot.salary
+from (select  employee_id,
+              first_name,
+              salary
+      from employees
+      order by salary desc) ot ;
+
+
+--rownum을 이용해서 원하는 순위의 값을 선택한다(where절)
+--where절이 2번 부터이면 데이터가 없다 (rownum이 항상 1이다)
+select  rownum ,
+        ot.employee_id,
+        ot.first_name,
+        ot.salary
+from (select  employee_id,
+              first_name,
+              salary
+      from employees
+      order by salary desc) ot  --정렬도 되어 있고  rownum로 있는 테이블이면?
+where rownum >= 2
+and rownum <= 5;
+
+-->(1)정렬하고 (2)rownum이 있는 테이블을 사용하고 (3)where절을 쓴다
+select  ort.rn,
+        ort.first_name,
+        ort.salary
+from (select  rownum rn,
+              ot.employee_id,
+              ot.first_name,
+              ot.salary
+      from (select  employee_id,
+                    first_name,
+                    salary
+            from employees
+            order by salary desc) ot   --(1)
+      )ort  --(2)
+where rn >= 2
+and rn<=5;  --(3)
+
+
+
+SELECT ROWNUM,
+        first_name,
+        salary
+FROM (select first_name, 
+                        salary
+                from employees
+                order by salary desc)
+where rownum >= 1 
+and rownum <= 5;
+
+
+--> 데이터가 없음 / 첫자리가 1이 아니면 못 구함 
+--> 이유 : 1에 데이터가 없는 경우 1이후 데이터들이 1의 자리에 들어감
+--> 따라서 계속 1의 데이터로 채워지고 조건은 1의 데이터가 아닌 
+--> 2부터의 데이터를 원하기 때문에 값이 없음 
+SELECT ROWNUM,
+        first_name,
+        salary
+FROM (select first_name, 
+                        salary
+                from employees
+                order by salary desc)
+where rownum >= 2 
+and rownum <= 3
 
 
